160e9c7950f6fa7bfc6725697964b301e16909bd
Kamil Sucharski <kamil.sucharski@sengami.com>
2019-03-21 19:20:00 +0100

    added smoother animation of loading indicator

--- feature/base/domain_base/src/main/java/com/sengami/domain_base/Constants.java
+++ feature/base/domain_base/src/main/java/com/sengami/domain_base/Constants.java
@@ -3,7 +3,7 @@ package com.sengami.domain_base;
 public class Constants {
     public static final String DATABASE_ALIAS = "my_astral_diary";
     public static final String DATABASE_EXTENSION = "db";
-    public static final String TEXT_FILE_EXTENSION = "db";
+    public static final String TEXT_FILE_EXTENSION = "txt";
     public static final String DATABASE_NAME = DATABASE_ALIAS + "." + DATABASE_EXTENSION;
     public static final String DATABASE_PATH = "databases/" + DATABASE_NAME;
     public static final String DATABASE_BACKUP_NAME_FORMAT = DATABASE_ALIAS + "_%1$s." + DATABASE_EXTENSION;
--- util/android/loading_indicator/src/main/java/com/sengami/util_loading_indicator/implementation/ViewVisibilityLoadingIndicator.java
+++ util/android/loading_indicator/src/main/java/com/sengami/util_loading_indicator/implementation/ViewVisibilityLoadingIndicator.java
@@ -1,5 +1,7 @@
 package com.sengami.util_loading_indicator.implementation;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.view.View;
 
 import com.sengami.domain_base.loading.LoadingIndicator;
@@ -10,13 +12,43 @@ public final class ViewVisibilityLoadingIndicator implements LoadingIndicator {
 
     @NotNull
     private final View view;
+    private final int animationDuration;
 
     public ViewVisibilityLoadingIndicator(@NotNull final View view) {
         this.view = view;
+        this.animationDuration = view
+            .getContext()
+            .getResources()
+            .getInteger(android.R.integer.config_shortAnimTime);
     }
 
     @Override
     public void setLoading(final boolean loading) {
-        view.setVisibility(loading ? View.VISIBLE : View.GONE);
+        if (loading) {
+            showWithAnimation();
+        } else {
+            hideWithAnimation();
+        }
+    }
+
+    private void showWithAnimation() {
+        view.setAlpha(0F);
+        view.setVisibility(View.VISIBLE);
+        view.animate()
+            .alpha(1F)
+            .setDuration(animationDuration)
+            .setListener(null);
+    }
+
+    private void hideWithAnimation() {
+        view.animate()
+            .alpha(0f)
+            .setDuration(animationDuration)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    view.setVisibility(View.GONE);
+                }
+            });
     }
 }
